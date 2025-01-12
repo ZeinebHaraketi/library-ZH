3f3505bf3dab2db03ca52101e086aa715cab1540
Zeineb Haraketi <zeineb.haraketi@esprit.tn>
2025-01-12 02:53:44 +0100

    initial commit

---
+++ lib/workflow.ts
@@ -0,0 +1,14 @@
+import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
+import config from "@/lib/config";
+
+export const workflowClient = new WorkflowClient({
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async () => {};
